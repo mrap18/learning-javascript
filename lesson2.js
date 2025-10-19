52cebcfaa4a8feffced013f436bb7cd92689fb28
mrap18 <mranuragpanthi7@gmail.com>
2025-10-19 09:37:04 +0545

    let const var

--- lesson2.js
+++ lesson2.js
@@ -1,2 +1,20 @@
-console.log("Anurag");
-console.log("Second commit");
+// console.log("Anurag");
+// console.log("Second commit");
+const accountId =  13579;
+let accountAddress = "Butwal";
+var accountNumber = "22331456";
+accountEmail = "xyz@gmail.com";
+let accountState;
+
+// accountId = 2468;
+accountAddress = "kathmandu";
+accountNumber = "66557162";
+accountEmail = "abc@gmail.com";
+
+/*
+Prefer not to use var in javaScript 
+because of issue in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountAddress,accountNumber,accountEmail,accountState]);
